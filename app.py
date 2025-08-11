528c2b0ba6c0996b30be8cf3d544867e31fd9236
Lucas Acelino <lucasaacelino@gmail.com>
2025-08-11 00:39:32 -0300

    refactor: ajusatndo o tempo de expiração do cache

--- app.py
+++ app.py
@@ -17,13 +17,6 @@ def index():
     except Exception as e:
         return f"Erro ao conectar: {e}"
 
-from datetime import datetime, date
-
-def json_serializer(obj):
-    """Serializa objetos que não são serializáveis por padrão"""
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    raise TypeError(f"Type {type(obj)} not serializable") 
 
 def getConnectionDB():
     conn = psycopg2.connect(
@@ -32,74 +25,28 @@ def getConnectionDB():
         dbname=os.environ["DB_NAME"],
         user=os.environ["DB_USER"],
         password=os.environ["DB_PASSWORD"],
-        # cursor_factory=RealDictCursor
     )
     
     return conn
 
 @app.get("/sobreviventes")
 def getTodasMusicasB():
-    
-    # conection = getConnectionDB()
-    
-    # try:
-    #     cacheaTodasMusicas = redis.get(CACHE_KEY)
-    #     if(cacheaTodasMusicas):
-    #         return jsonify(json.loads(cacheaTodasMusicas))
-        
-    #     conection = getConnectionDB()
-    #     cursor = conection.cursor()
-    #     cursor.execute("SELECT * FROM spotifySongs")
-    #     musicas = cursor.fetchall()
-        
-    #     redis.setex(CACHE_KEY, 120, json.dumps(musicas))
-        
-    #     conection.close()
-        
-    #     return {"músicas": musicas}
-    # try:
-    #     # Verifica cache primeiro
-    #     cached_musicas = redis.get(CACHE_KEY)
-    #     if cached_musicas:
-    #         return jsonify(json.loads(cached_musicas))
-        
-    #     # Se não tem cache, busca no banco
-    #     connection = getConnectionDB()
-    #     cursor = connection.cursor()
-        
-    #     cursor.execute("SELECT * FROM spotifySongs")
-    #     columns = [desc[0] for desc in cursor.description]  # Pega os nomes das colunas
-    #     musicas = [dict(zip(columns, row)) for row in cursor.fetchall()]  # Converte para dict
-        
-    #     # Armazena no cache
-    #     redis.setex(CACHE_KEY, 120, json.dumps(musicas))
-        
-    #     return jsonify({"musicas": musicas})
-    
-    # except Exception as e:
-    #     return jsonify({"erro": str(e)}), 500
-    
     connection = None
     try:
-        # Verifica cache primeiro
         cached_sobreviventes = redis.get(CACHE_KEY)
         if cached_sobreviventes:
             return jsonify(json.loads(cached_sobreviventes))
         
-        # Se não tem cache, busca no banco
         connection = getConnectionDB()
         cursor = connection.cursor()
         
         cursor.execute("SELECT * FROM sobreviventes")
         columns = [desc[0] for desc in cursor.description]
-        musicas = [dict(zip(columns, row)) for row in cursor.fetchall()]# Pega os nomes das colunas
+        sobreviventes = [dict(zip(columns, row)) for row in cursor.fetchall()]
         
-        # Converte para dict e serializa campos datetime
+        redis.setex(CACHE_KEY, 120, json.dumps(sobreviventes))
         
-        # Armazena no cache com o serializador personalizado
-        redis.setex(CACHE_KEY, 120, json.dumps(musicas))
-        
-        return jsonify({"musicas": musicas})
+        return jsonify(sobreviventes)
     
     except Exception as e:
         return jsonify({"erro": str(e)}), 500
